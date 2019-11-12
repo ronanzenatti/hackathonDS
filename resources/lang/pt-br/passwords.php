17324566d015130cd753c45ae815bc50227f9192
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2019-11-12 17:52:24 -0300

    Tradução para pt-br

---
+++ resources/lang/pt-br/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password'  => 'A senha deve conter pelo menos 8 caracteres.',
+    'reset'     => 'Sua senha foi redefinida!',
+    'sent'      => 'Enviamos um link para redefinir a sua senha por e-mail.',
+    'throttled' => 'Por favor aguarde para entrar novamente.',
+    'token'     => 'Código de redefinição de senha é inválido.',
+    'user'      => 'Não encontramos o endereço de e-mail informado.',
+];
